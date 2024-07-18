36daafc54521b89575d97a18b762059c34b88e28
andreibojor <bojor.andrei@gmail.com>
2024-07-18 18:50:50 +0300

    sadsadsa

---
+++ prettier.config.mjs
@@ -0,0 +1,31 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  tailwindConfig: './tailwind.config.ts',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/',
+    '^[../]',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '4.4.0',
+};
+
+export default config;
+
+// from prettierrc.json
+// {
+//   "singleQuote": true,
+//   "trailingComma": "all",
+//   "printWidth": 100,
+//   "semi": false
+// }
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import './globals.scss'
-import { Inter } from 'next/font/google'
-import type { Metadata } from 'next'
+import React from 'react';
+
+import './globals.scss';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Digital Marketplace',
   description:
     'A digital marketplace built with Next.JS, Payload, using Supabase as a database and hosted on Vercel',
-}
+};
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 /* Our app sits here to not cause any conflicts with payload's root layout  */
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html className={inter.className}>
-      <body className="min-h-screen antialiased">
+      <body className="relative flex min-h-screen flex-col items-center justify-center antialiased">
         <main className="flex min-h-screen max-w-7xl flex-col items-center justify-center">
           {children}
         </main>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
