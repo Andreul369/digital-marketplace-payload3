dcad59e4db353b00f59b25d075a036cdf3d1817d
andreibojor <bojor.andrei@gmail.com>
2024-07-18 11:16:04 +0300

    first or second commit idk whatever push

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js config here
+  experimental: {
+    reactCompiler: false,
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
+  },
+}
+
+export default withPayload(nextConfig)
