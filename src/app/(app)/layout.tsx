33952a5c94acb641ac15384960c3f21fe1395b27
andreibojor <bojor.andrei@gmail.com>
2024-07-18 13:15:51 +0300

    added metadata tag, cn function, and shadcn button

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './globals.scss'
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 })
+
+export const metadata: Metadata = {
+  title: 'Digital Marketplace',
+  description:
+    'A digital marketplace built with Next.JS, Payload, using Supabase as a database and hosted on Vercel',
+}
 
 /* Our app sits here to not cause any conflicts with payload's root layout  */
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
