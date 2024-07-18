bcacb299508e61745ded7f699c2b861c28df5152
andreibojor <bojor.andrei@gmail.com>
2024-07-18 13:31:03 +0300

    addded more shadcn components

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,22 +3,22 @@ import './globals.scss'
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 export const metadata: Metadata = {
   title: 'Digital Marketplace',
   description:
     'A digital marketplace built with Next.JS, Payload, using Supabase as a database and hosted on Vercel',
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 /* Our app sits here to not cause any conflicts with payload's root layout  */
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html className={inter.className}>
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,81 @@
-export { Button, buttonVariants } from './button'
+export { Button, buttonVariants } from './button';
+
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './card';
+
+export {
+  type CarouselApi,
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from './carousel';
+
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
+
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './form';
+
+export { Input } from './input';
+
+export { Label } from './label';
+
+export { ScrollArea, ScrollBar } from './scrollarea';
+
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from './select';
+
+export {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from './sheet';
+
+export { Toaster } from './sonner';
